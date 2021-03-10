3b86d1e9e2ad8998a6cc108aec5ab15912cab22a
Ramesh <rameswarudu.naraboina@team.telstra.com>
2021-03-10 10:46:33 +0530

    commit and push by murali

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $orangeHRM999.orangeHRM999;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
